a3600c9059aab87ce2c7a81e6c06e3b15625b1f0
Korada Saikiran <80874246+saikiran76@users.noreply.github.com>
2026-07-06 14:18:12 +0530

    Update Linear regression training (Math Foundations 6/6)

--- foundations/linear_regression_training.py
+++ foundations/linear_regression_training.py
@@ -5,10 +5,12 @@ from numpy.typing import NDArray
 class Solution:
     def get_derivative(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64], N: int, X: NDArray[np.float64], desired_weight: int) -> float:
         # note that N is just len(X)
-        return -2 * np.dot(ground_truth - model_prediction, X[:, desired_weight]) / N
-
+        return -2/N * np.sum((ground_truth - model_prediction)*(X[:, desired_weight]))
+       
     def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
-        return np.squeeze(np.matmul(X, weights))
+        Y = np.dot(X, weights)
+        return np.squeeze(Y)
+        
 
     learning_rate = 0.01
 
@@ -24,27 +26,24 @@ class Solution:
         #   2. For each weight index j, compute gradient with get_derivative()
         #   3. Update: weights[j] -= learning_rate * gradient
         # Return np.round(final_weights, 5)
-        curr_weights = np.array(initial_weights, dtype=np.float64)
-        # model_predictions = self.get_model_prediction(X, curr_weights)
-        # grad = get_derivative(X, model_predictions, )
-        # for i in range(len(curr_weights)):
-        #     gradient = self.get_derivative(model_predictions, Y, len(X), X, i)
-        #     curr_weights[i] = curr_weights[i] - self.learning_rate*gradient
+        weights = np.array(initial_weights, dtype=np.float64)
         N = len(X)
-
-        for _ in range(num_iterations): # epochs
-            # 2. Re-calculate predictions at the start of every iteration with the updated weights
-            model_predictions = self.get_model_prediction(X, curr_weights)
-            
-            # 3. Calculate gradients and update weights
-            for i in range(len(curr_weights)):
-                gradient = self.get_derivative(model_predictions, Y, N, X, i)
-                curr_weights[i] = curr_weights[i] - self.learning_rate * gradient
         
-        return np.round(curr_weights, 5)
+
+        for i in range(num_iterations):
+            pred = self.get_model_prediction(X, weights)
+            gradients = np.zeros_like(weights)
+
+            for j in range(len(weights)):
+                gradients[j] = self.get_derivative(pred, Y, N, X, j)
+            for j in range(len(weights)):
+                weights[j] -= self.learning_rate*gradients[j]
+        return np.round(weights, 5)
+
+        
+        
+        
 
 
-        # for _ in range(iterations):
-        #     gradient = get_gradient()
-        #     curr_weights = curr_weights - learning_rate*
+        
 
