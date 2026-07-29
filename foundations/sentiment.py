95a64784b220125469b599b2a73a2d69c705ad03
Korada Saikiran <80874246+saikiran76@users.noreply.github.com>
2026-07-29 23:48:14 +0530

    Update Sentiment analysis (NLP 3/4)

--- foundations/sentiment.py
+++ foundations/sentiment.py
@@ -7,17 +7,18 @@ class Solution(nn.Module):
         super().__init__()
         torch.manual_seed(0)
         # Layers: Embedding(vocabulary_size, 16) -> Linear(16, 1) -> Sigmoid
-        self.embedding = nn.Embedding(vocabulary_size, 16)
-        self.linear = nn.Linear(16, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.l1 = nn.Embedding(vocabulary_size, 16)
+        self.l2 = nn.Linear(16, 1)
+        self.l3 = nn.Sigmoid()
+    
 
     def forward(self, x: TensorType[int]) -> TensorType[float]:
         # Hint: The embedding layer outputs a B, T, embed_dim tensor
         # but you should average it into a B, embed_dim tensor before using the Linear layer
+        x = self.l1(x)
+        x = x.mean(dim=1)
+        x = self.l2(x)
+        x = self.l3(x)
 
         # Return a B, 1 tensor and round to 4 decimal places
-        out = self.embedding(x)
-        out = torch.mean(out, dim=1)
-        out = self.linear(out)
-        out = self.sigmoid(out)
-        return torch.round(out, decimals=4)
+        return torch.round(x, decimals=4)
